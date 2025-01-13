e66fd44cf5b9d4ccd9ac034286a3ed316fb3ec07
Bruno Fortunatti <bruno.fortunatti4170@gmail.com>
2025-01-13 17:08:23 -0300

    first styles

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
+/*import * as express from 'express';
+import { Request, Response } from 'express';
+import * as path from 'path';*/
 var app = express();
 var port = parseInt(process.env.PORT || "3000", 10);
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 app.listen(port, function () {
     console.log("Server is running at http://localhost:".concat(port));
 });
--- server.ts
+++ server.ts
@@ -1,14 +1,9 @@
-import * as express from 'express';
-import { Request, Response } from 'express';
-import * as path from 'path';
+const express = require('express');
+const path = require('path');
 const app = express();
 const port: number = parseInt(process.env.PORT || "3000", 10);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
