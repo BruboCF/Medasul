1b0f0788cf49cca77634f86ecf33d8cc14c7d90c
Bruno-PC-empresa <bruno.fortunatti4170@gmail.com>
2025-01-14 10:50:44 -0300

    styles cards and descriptions

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var path = require('path');
-/*import * as express from 'express';
-import { Request, Response } from 'express';
-import * as path from 'path';*/
 var app = express();
 var port = parseInt(process.env.PORT || "3000", 10);
 app.use(express.static(path.join(__dirname, 'public')));
