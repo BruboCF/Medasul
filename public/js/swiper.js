a44a217bbd63ef8a0689068963c97d8bce117804
Bruno-PC-empresa <bruno.fortunatti4170@gmail.com>
2025-01-16 11:02:20 -0300

    swiper working

---
+++ public/js/swiper.js
@@ -0,0 +1,111 @@
+const swiper1 = new Swiper("#carrossel1 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel1 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel1 .swiper-button-next",
+        prevEl: "#carrossel1 .swiper-button-prev",
+    },
+});
+
+const swiper2 = new Swiper("#carrossel2 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel2 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel2 .swiper-button-next",
+        prevEl: "#carrossel2 .swiper-button-prev",
+    },
+});
+
+const swiper3 = new Swiper("#carrossel3 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel3 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel3 .swiper-button-next",
+        prevEl: "#carrossel3 .swiper-button-prev",
+    },
+});
+
+const swiper4 = new Swiper("#carrossel4 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel4 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel4 .swiper-button-next",
+        prevEl: "#carrossel4 .swiper-button-prev",
+    },
+});
+
+const swiper5 = new Swiper("#carrossel5 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel5 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel5 .swiper-button-next",
+        prevEl: "#carrossel5 .swiper-button-prev",
+    },
+});
+
+const swiper6 = new Swiper("#carrossel6 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel6 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel6 .swiper-button-next",
+        prevEl: "#carrossel6 .swiper-button-prev",
+    },
+});
+
+const swiper7 = new Swiper("#carrossel7 .mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: "#carrossel7 .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: "#carrossel7 .swiper-button-next",
+        prevEl: "#carrossel7 .swiper-button-prev",
+    },
+});
