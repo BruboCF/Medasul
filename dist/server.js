639c9c12d62942c8c2d8101199ff8a22bd5b291b
Bruno Fortunatti <bruno.fortunatti4170@gmail.com>
2025-02-21 16:58:49 -0300

    deploy test

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = process.env.PORT || "3000";
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
